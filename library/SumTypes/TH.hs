7c45f9e140b96ecd7609eda59202205a6cd83f00
David Reaver <johndreaver@gmail.com>
2017-05-29 09:36:33 -0700

    Created sum type conversion functions

--- library/SumTypes/TH.hs
+++ library/SumTypes/TH.hs
@@ -1,20 +1,24 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module SumTypes.TH
-  ( constructSumType
+  ( -- * Constructing sum types
+    constructSumType
   , SumTypeOptions
   , defaultSumTypeOptions
   , sumTypeOptionsTagOptions
   , SumTypeTagOptions (..)
   , sumTypeOptionsConstructorStrictness
   , SumTypeConstructorStrictness (..)
+    -- * Converting between sum types
+  , sumTypeConverter
+  , partialSumTypeConverter
   ) where
 
 import Language.Haskell.TH
 
 -- | This is a template haskell function that creates a sum type from a list of
--- types. This is very useful when creating an event type for a 'Projection'
--- from a list of events in your system. Here is an example:
+-- types. Here is an example:
 --
 -- > data TypeA = TypeA
 -- > data TypeB = TypeB
@@ -34,14 +38,14 @@ import Language.Haskell.TH
 -- > deriving instance Show MySum
 -- > deriving instance Eq MySum
 constructSumType :: String -> SumTypeOptions -> [Name] -> Q [Dec]
-constructSumType typeName SumTypeOptions{..} eventTypes = do
+constructSumType typeName SumTypeOptions{..} types = do
   let
     strictness = constructorStrictness sumTypeOptionsConstructorStrictness
-    mkConstructor eventName =
+    mkConstructor name =
       NormalC
-      (constructorName sumTypeOptionsTagOptions typeName eventName)
-      [(Bang NoSourceUnpackedness strictness, ConT eventName)]
-    constructors = map mkConstructor eventTypes
+      (constructorName sumTypeOptionsTagOptions typeName name)
+      [(Bang NoSourceUnpackedness strictness, ConT name)]
+    constructors = map mkConstructor types
   return [DataD [] (mkName typeName) [] Nothing constructors []]
 
 -- | Options for 'constructSumType'. Note that the constructor for this type is
@@ -97,3 +101,127 @@ data SumTypeConstructorStrictness
 constructorStrictness :: SumTypeConstructorStrictness -> SourceStrictness
 constructorStrictness LazySumTypeConstructors = NoSourceStrictness
 constructorStrictness StrictSumTypeConstructors = SourceStrict
+
+-- | This template haskell function creates a conversion function between two
+-- sum types. It works by matching up constructors that share the same inner
+-- type. Note that all types in the source sum type must be present in the
+-- target sum type, or you will get an error.
+--
+-- > data MySum
+-- >   = MySumTypeA TypeA
+-- >   | MySumTypeB TypeB
+-- >   | MySumTypeC TypeC
+-- >
+-- > data OtherSum
+-- >   = OtherSumTypeA TypeA
+-- >   | OtherSumTypeB TypeB
+-- >
+-- > sumTypeConverter "otherSumToMySum" ''OtherSum ''MySum
+--
+-- This will producing the following code:
+--
+-- > otherSumToMySum :: OtherSum -> MySum
+-- > otherSumToMySum (OtherSumTypeA typeA) = MySumTypeA typeA
+-- > otherSumToMySum (OtherSumTypeB typeB) = MySumTypeB typeB
+sumTypeConverter :: String -> Name -> Name -> Q [Dec]
+sumTypeConverter functionName sourceType targetType = do
+  bothConstructors <- matchTypeConstructors sourceType targetType
+  let
+    funcName = mkName functionName
+  funcClauses <- mapM mkSerializeFunc bothConstructors
+  typeDecl <- [t| $(conT sourceType) -> $(conT targetType) |]
+  return
+    [ SigD funcName typeDecl
+    , FunD funcName funcClauses
+    ]
+
+-- | Similar to 'sumTypeConverter', except not all types in the source sum type
+-- need to be present in the target sum type.
+--
+-- > data MySum
+-- >   = MySumTypeA TypeA
+-- >   | MySumTypeB TypeB
+-- >   | MySumTypeC TypeC
+-- >
+-- > data OtherSum
+-- >   = OtherSumTypeA TypeA
+-- >   | OtherSumTypeB TypeB
+-- >
+-- > partialSumTypeConverter "mySumToOtherSum" ''MySum ''OtherSum
+--
+-- This will producing the following code:
+--
+-- > mySumToOtherSum :: MySum -> Maybe OtherSum
+-- > mySumToOtherSum (MySumTypeA typeA) = Just $ OtherSumTypeA typeA
+-- > mySumToOtherSum (MySumTypeB typeB) = Just $ OtherSumTypeB typeB
+-- > mySumToOtherSum other = Nothing
+partialSumTypeConverter :: String -> Name -> Name -> Q [Dec]
+partialSumTypeConverter functionName sourceType targetType = do
+  bothConstructors <- matchTypeConstructors targetType sourceType
+  let
+    funcName = mkName functionName
+    wildcardClause = Clause [WildP] (NormalB (ConE 'Nothing)) []
+  funcClauses <- mapM mkDeserializeFunc bothConstructors
+  typeDecl <- [t| $(conT sourceType) -> Maybe $(conT targetType) |]
+
+  return
+    [ SigD funcName typeDecl
+    , FunD funcName (funcClauses ++ [wildcardClause])
+    ]
+
+matchTypeConstructors :: Name -> Name -> Q [BothConstructors]
+matchTypeConstructors sourceType targetType = do
+  sourceConstructors <- typeConstructors sourceType
+  targetConstructors <- typeConstructors targetType
+  mapM (matchConstructor targetConstructors) sourceConstructors
+
+-- | Extract the constructors and types for the given sum type.
+typeConstructors :: Name -> Q [(Type, Name)]
+typeConstructors typeName = do
+  info <- reify typeName
+  case info of
+    (TyConI (DataD _ _ _ _ constructors _)) -> mapM go constructors
+      where
+        go (NormalC name []) = fail $ "Constructor " ++ nameBase name ++ " doesn't have any arguments"
+        go (NormalC name [(_, type')]) = return (type', name)
+        go (NormalC name _) = fail $ "Constructor " ++ nameBase name ++ " has more than one argument"
+        go _ = fail $ "Invalid constructor in " ++ nameBase typeName
+    _ -> fail $ nameBase typeName ++ " must be a sum type"
+
+-- | Find the corresponding target constructor for a given source constructor.
+matchConstructor :: [(Type, Name)] -> (Type, Name) -> Q BothConstructors
+matchConstructor targetConstructors (type', sourceConstructor) = do
+  targetConstructor <-
+    maybe
+    (fail $ "Can't find constructor in target type corresponding to " ++ nameBase sourceConstructor)
+    return
+    (lookup type' targetConstructors)
+  return $ BothConstructors type' sourceConstructor targetConstructor
+
+-- | Utility type to hold the source and target constructors for a given type.
+data BothConstructors =
+  BothConstructors
+  { innerType :: Type
+  , sourceConstructor :: Name
+  , targetConstructor :: Name
+  }
+
+-- | Construct the TH function 'Clause' for the serialization function for a
+-- given type.
+mkSerializeFunc :: BothConstructors -> Q Clause
+mkSerializeFunc BothConstructors{..} = do
+  varName <- newName "value"
+  let
+    patternMatch = ConP sourceConstructor [VarP varName]
+    constructor = AppE (ConE targetConstructor) (VarE varName)
+  return $ Clause [patternMatch] (NormalB constructor) []
+
+-- | Construct the TH function 'Clause' for the deserialization function for a
+-- given type.
+mkDeserializeFunc :: BothConstructors -> Q Clause
+mkDeserializeFunc BothConstructors{..} = do
+  varName <- newName "value"
+  let
+    patternMatch = ConP targetConstructor [VarP varName]
+    constructor = AppE (ConE 'Just) (AppE (ConE sourceConstructor) (VarE varName))
+  return $ Clause [patternMatch] (NormalB constructor) []
--- tests/SumTypes/THSpec.hs
+++ tests/SumTypes/THSpec.hs
@@ -12,13 +12,30 @@ data TypeB = TypeB deriving (Show, Eq)
 data TypeC = TypeC deriving (Show, Eq)
 
 constructSumType "MySum" defaultSumTypeOptions [''TypeA, ''TypeB, ''TypeC]
+constructSumType "OtherSum" defaultSumTypeOptions [''TypeA, ''TypeB]
+sumTypeConverter "otherSumToMySum" ''OtherSum ''MySum
+partialSumTypeConverter "mySumToOtherSum" ''MySum ''OtherSum
 
 deriving instance Show MySum
 deriving instance Eq MySum
 
+deriving instance Show OtherSum
+deriving instance Eq OtherSum
+
 spec :: Spec
 spec = do
-  describe "mkSumType" $ do
-    it "can create events" $ do
+  describe "constructSumType" $ do
+    it "compiles without breaking" $ do
       -- The real utility in this test is the fact that it compiles
       length [MySumTypeA TypeA, MySumTypeB TypeB, MySumTypeC TypeC] `shouldBe` 3
+
+  describe "sumTypeConverter" $ do
+    it "properly converts between types" $ do
+      otherSumToMySum (OtherSumTypeA TypeA) `shouldBe` MySumTypeA TypeA
+      otherSumToMySum (OtherSumTypeB TypeB) `shouldBe` MySumTypeB TypeB
+
+  describe "partialSumTypeConverter" $ do
+    it "properly converts between types" $ do
+      mySumToOtherSum (MySumTypeA TypeA) `shouldBe` Just (OtherSumTypeA TypeA)
+      mySumToOtherSum (MySumTypeB TypeB) `shouldBe` Just (OtherSumTypeB TypeB)
+      mySumToOtherSum (MySumTypeC TypeC) `shouldBe` Nothing
